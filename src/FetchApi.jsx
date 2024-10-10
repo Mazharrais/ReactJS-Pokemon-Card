94ad897e737794a9b2106e5bd5ad599602f127ce
Mazharrais <mazharmajo72@gmail.com>
2024-10-10 14:30:04 +0500

    add code

--- src/FetchApi.jsx
+++ src/FetchApi.jsx
@@ -7,14 +7,22 @@ const FetchApi = () => {
 
 
   const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
 
   const fetchPokemon = () =>{
     fetch(API)
     .then((res) => res.json())
-    .then((data) => setApiData(data))
-    .catch((error)=> console.log('error', error))
+    .then((data) => {setApiData(data)
+    setLoading(false)})
+    .catch((error)=>{ 
+        console.log('error', error);
+        setError(error)
+        console.log(false);
+        
+    })
   }
 
      useEffect(()=>{
@@ -25,9 +33,26 @@ const FetchApi = () => {
 
     console.log(apiData);
     
+    if(loading)
+        return(
+       <div>
+           <h1>loading...</h1>
+       </div>
+        );
+
+        if(error)
+            return(
+           <div>
+            <h1>Error : {error.message}</h1>
+           </div>
+        
+
+        )
     
     
- if(apiData){
+//   if(apiData){
+
+ 
 
      return (
          <section className='container'>
@@ -42,11 +67,23 @@ const FetchApi = () => {
                 className='pokemon-image' />
             </figure>
             <h1>{apiData.name}</h1>
+            <div className='grid-three-cols'>
+                <p className='pokemon-info'>
+                    Height: <span>{apiData.height}</span>
+                </p>
+                <p className='pokemon-info'>
+                    Weight: <span>{apiData.weight}</span>
+                </p>
+                <p className='pokemon-info'>
+                    Speed: <span>{apiData.stats[5].base_stat}</span>
+                </p>
+            </div>
         </li>
     </ul>
     </section>
   )
 }
-}
+// }
+
 
 export default FetchApi;
