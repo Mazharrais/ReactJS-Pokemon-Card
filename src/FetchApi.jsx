5df7e610b76ebbab2d1b1aa538b2460cfe310639
Mazharrais <mazharmajo72@gmail.com>
2024-10-10 13:02:40 +0500

    add changes

--- src/FetchApi.jsx
+++ src/FetchApi.jsx
@@ -1,38 +1,52 @@
 
+import './pokemon.css';
+import React, { useEffect, useState } from 'react';
 
-import React, { useEffect, useState } from 'react'
 
 const FetchApi = () => {
 
 
-  const [apiData, setApiData] = useState([]);
+  const [apiData, setApiData] = useState(null);
 
-     useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/posts")
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+  const fetchPokemon = () =>{
+    fetch(API)
     .then((res) => res.json())
     .then((data) => setApiData(data))
     .catch((error)=> console.log('error', error))
+  }
+
+     useEffect(()=>{
+    fetchPokemon();
+
      },[])
     
 
+    console.log(apiData);
     
     
     
+ if(apiData){
 
-  return (
-    <>
-     <ul>
-        Data :
-        {
-            apiData.map((currData)=>{
-                return <li key={currData.id}>
-                  {currData.title}
-                </li>
-            })
-        }
-     </ul>
-    </>
+     return (
+         <section className='container'>
+    <header>
+        <h1>Let's Catch Pokemon...!</h1>
+    </header>
+    <ul className='card-demo'>
+        <li className='pokemon-card'>
+            <figure>
+                <img src={apiData.sprites.other.dream_world.front_default}
+                alt={apiData.name}
+                className='pokemon-image' />
+            </figure>
+            <h1>{apiData.name}</h1>
+        </li>
+    </ul>
+    </section>
   )
+}
 }
 
 export default FetchApi;
